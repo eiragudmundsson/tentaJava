3956d0a923ee62052d2367070d453d251d474e5e
eiragudmundsson <42918060+eiragudmundsson@users.noreply.github.com>
2022-11-02 14:31:02 +0100

    minor update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import myModule.ReadInOut;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Write some words, write stop when done.");
+
         ReadInOut r = new ReadInOut();
     }
 }
